e45b9f13e4c68a3bd2dd10ce06a2b1b8a2d6c31f
Huanhuan Guo (WUZ) <huanhuan.guo1@flex.com>
2020-05-18 08:49:48 +0800

    基本折线图

---
+++ line_plotly/siample_line.py
@@ -0,0 +1,9 @@
+﻿import plotly.express as px
+
+df = px.data.gapminder().query("country=='Canada'")
+print(df)
+fig = px.line(df, x="year", y="lifeExp", title='Life expectancy in Canada')
+
+fig.write_json(file="./plotlyjs/data.json")
+
+
