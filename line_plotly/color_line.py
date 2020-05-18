91d9e44c58ebb0aac3c9055615e24ec31653ad93
Huanhuan Guo (WUZ) <huanhuan.guo1@flex.com>
2020-05-18 09:00:34 +0800

    设置折线图颜色

---
+++ line_plotly/color_line.py
@@ -0,0 +1,8 @@
+﻿import plotly.express as px
+
+df = px.data.gapminder().query("continent=='Oceania'")
+fig = px.line(df, x="year", y="lifeExp", color='country')
+
+fig.write_json(file="./plotlyjs/data.json")
+
+
