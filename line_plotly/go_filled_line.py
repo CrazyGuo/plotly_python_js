7da64719447b464b2c5f7aca4663e3e6436f05d4
Huanhuan Guo (WUZ) <huanhuan.guo1@flex.com>
2020-05-19 10:24:28 +0800

    多条线填充

---
+++ line_plotly/go_filled_line.py
@@ -0,0 +1,53 @@
+﻿import plotly.graph_objects as go
+import numpy as np
+
+
+
+x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+x_rev = x[::-1]
+
+# Line 1
+y1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+y1_upper = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+y1_lower = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+y1_lower = y1_lower[::-1]
+
+# Line 2
+y2 = [5, 2.5, 5, 7.5, 5, 2.5, 7.5, 4.5, 5.5, 5]
+y2_upper = [5.5, 3, 5.5, 8, 6, 3, 8, 5, 6, 5.5]
+y2_lower = [4.5, 2, 4.4, 7, 4, 2, 7, 4, 5, 4.75]
+y2_lower = y2_lower[::-1]
+
+# Line 3
+y3 = [10, 8, 6, 4, 2, 0, 2, 4, 2, 0]
+y3_upper = [11, 9, 7, 5, 3, 1, 3, 5, 3, 1]
+y3_lower = [9, 7, 5, 3, 1, -.5, 1, 3, 1, -1]
+y3_lower = y3_lower[::-1]
+
+xL = x + x_rev
+yL = y1_upper + y1_lower
+
+print(xL)
+print(yL)
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(
+    x=x+x_rev,
+    y=y1_upper+y1_lower,
+    fill='toself',
+    fillcolor='rgba(0,100,80,0.2)',
+    line_color='rgba(255,255,255,0)',
+    showlegend=False,
+    name='Fair',
+))
+
+fig.add_trace(go.Scatter(
+    x=x, y=y1,
+    line_color='rgb(0,100,80)',
+    name='Fair',
+))
+
+fig.write_json(file="./plotlyjs/data.json")
+
+
