30a7fa4c36e5396baf10d60d4cb31f44d78399be
Huanhuan Guo (WUZ) <huanhuan.guo1@flex.com>
2020-05-25 11:17:22 +0800

    自定义柱状图的颜色与悬浮显示

---
+++ bar_plotly/customize_bar.py
@@ -0,0 +1,13 @@
+﻿import plotly.express as px
+
+data = px.data.gapminder()
+
+data_canada = data[data.country == 'Canada']
+#默认对应的x,y轴对应的值会同hover_data一样，悬浮的时候显示，同时labels可以定制字段的显示
+fig = px.bar(data_canada, x='year', y='pop',
+             hover_data=['lifeExp', 'gdpPercap'], color='lifeExp',
+             labels={'pop':'population of Canada'}, height=400)
+
+fig.write_json(file="./plotlyjs/data.json")
+
+
