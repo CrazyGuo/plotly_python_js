c6d70f8cc8ca8fa26d284b3dc8052e18b99b37a4
Huanhuan Guo (WUZ) <huanhuan.guo1@flex.com>
2020-06-04 15:22:38 +0800

    int数据类型精度损失问题

---
+++ think_python/number_test.py
@@ -0,0 +1,16 @@
+val = 123.22223
+
+temp_vlaue = "%.2f" % (val * 100)
+
+result = temp_vlaue + '%'
+print(temp_vlaue)
+
+current_mlvc_gap = 1.16
+r1 = current_mlvc_gap * 100
+print(r1)
+test_a = str(int(current_mlvc_gap * 100))
+print(test_a)
+
+current_mlvc_gap_show = str(test_a) + '%'
+
+print(current_mlvc_gap_show)
