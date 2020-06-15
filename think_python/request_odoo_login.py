65a520c2d4c7aad851a9224196abf4e803f568aa
Huanhuan Guo (WUZ) <huanhuan.guo1@flex.com>
2020-06-15 13:08:00 +0800

    测试odoo的login接口

---
+++ think_python/request_odoo_login.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import requests
+
+headers = {'Content-Type': 'application/json'}
+
+url = "http://127.0.0.1:8069/api/ionic/login_auth"
+
+params = { "params": {"login":"adm", "pwd":"adm123",  } , "db":"mps2"}
+
+r = requests.post(url, json=params, headers=headers)
+
+print(r.text)
