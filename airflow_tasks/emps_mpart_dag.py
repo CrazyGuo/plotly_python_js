71cdf544f560d191f219add2ef9c2b407afd9cc7
Huanhuan Guo (WUZ) <huanhuan.guo1@flex.com>
2021-01-28 08:50:43 +0800

    test airflow dag file

---
+++ airflow_tasks/emps_mpart_dag.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import os
+
+from airflow.models import DAG
+from airflow.operators.python_operator import PythonOperator
+
+#docker voulme location: /home/wuzlxadmin/airflow_server/airflow_dags  -->  /usr/local/airflow/dags
+PYTHON_TARGET_File = "/usr/local/airflow/dags" + "/invoked_files/emps_mpart_items.py"
+
+default_args = {
+    "owner": "huanhuan.guo",
+    "start_date": datetime(2021, 1, 26)
+}
+
+dag = DAG("eMPS_Mpart_Items", 
+        description="Get mpart items from baan, insert to FlexPsApp.dbo.FlexMpsMpartItems",
+        default_args=default_args, 
+        schedule_interval='30 06 * * * *')
+
+def invoke_python_file():
+    cmmd = "%s %s" % ("python", PYTHON_TARGET_File)
+    os.system(cmmd)
+
+t1 = PythonOperator(task_id="emps_mpart_item_task", python_callable=invoke_python_file, retries=0, dag=dag)
---
+++ airflow_tasks/emps_mpart_items.py
@@ -0,0 +1,68 @@
+import datetime
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, String, DateTime,Float, Integer, create_engine
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+BATCH_INSERT_COUNT = 500
+
+
+class FlexMpsMpartItem(Base):
+    __tablename__ = 'FlexMpsMpartItems'
+
+    id = Column(Integer, primary_key=True, index=True)
+    Item = Column(String(20))
+    CreatedBy = Column(Integer, default=1)
+    CreatedTime = Column(DateTime, default=datetime.datetime.utcnow)
+    UpdatedBy = Column(Integer, default=1)
+    UpdatedTime = Column(DateTime, default=datetime.datetime.utcnow)
+
+
+def get_oracle_data():
+    db_engine = create_engine('oracle://READ_898:Flexbaan_898@am3p1rpt.flex.com:1571/am3p1', encoding="utf-8", max_identifier_length=30)
+    sql = """
+                select trim(A.item) as item
+                from bo_read898.tcibd001 A
+                where A.kitm = '2'
+                and (trim(A.item) not like 'HPM%'
+                and trim(A.item) not like 'DEL%'
+                and trim(A.item) not like 'LNV%')
+                and (A.csig = 'IAI' or A.csig = '   ' or A.csig = 'CAH')
+    """
+
+    connection = db_engine.connect()
+    result = connection.execute(sql)
+    return result
+
+def insert_to_mssql(data_list):
+    db_engine = create_engine('mssql+pymssql://gm:flex@gmwz@10.201.152.22/FlexPSApp', encoding="utf-8")
+    db_session = sessionmaker(bind=db_engine)
+    session = db_session() 
+
+    truncate_table_sql = "truncate table FlexMpsMpartItems"
+    session.execute(truncate_table_sql, params={})
+
+    length = 0
+    for row in data_list:
+        item = FlexMpsMpartItem(Item=row['item'])
+        session.add(item)
+        length += 1
+
+        if length == BATCH_INSERT_COUNT:
+            session.flush()
+            session.commit()
+            length = 0
+    if length > 0 :
+        session.flush()
+        session.commit() 
+    session.close() 
+
+def task_job():
+    data_list = get_oracle_data()
+    insert_to_mssql(data_list)
+
+if __name__ == '__main__':
+    task_job()
+else:
+    task_job()
